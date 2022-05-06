c8e993e97defe7b8a7957509b12a19a800a98e12
AphisitTham <aphisit.earth@gmail.com>
2022-05-06 23:44:42 +0700

    add text title

--- src/views/home.js
+++ src/views/home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() { 
         return (
-            <h1>Welcome to the moon</h1>
+            <h1>Welcome to the moon. Hello</h1>
         );
     }
 }
